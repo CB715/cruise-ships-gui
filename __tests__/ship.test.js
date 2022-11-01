c40ff662457c23fbf2c89993bc0f08e09450ed11
Carl Beckley <carlbeckley@gmail.com>
2022-11-01 13:27:51 +0000

    added new port object and function to set sail

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -1,12 +1,31 @@
 /* globals describe it expect */
-const Ship = require('../src/ship');
+const { Ship, Port } = require('../src/ship');
 
-describe('Creating an object', () => {
+describe('Creating a ship object', () => {
     it('return a new ship object', () => {
         expect(new Ship()).toBeInstanceOf(Object);
     });
-    it('should have a starting point', () => {
+    it('should have a port', () => {
         const ship = new Ship();
-        expect(ship.startingPort).toEqual('Liverpool');
+        expect(ship.currentPort).toEqual('Liverpool');
     });
 });
+
+describe('Setting sail method of Ship object', () => {
+    it('sets sail from the starting port', () => {
+        const ship = new Ship();
+        ship.setSail ();
+        expect(ship.currentPort).toBeNull();
+    });
+});
+
+describe('Creating a port object', () => {
+    it('return a new port object', () => {
+        expect(new Port()).toBeInstanceOf(Object);
+    });
+    it('gives the port a name', () => {
+        const port = new Port('Belfast');
+        expect(port.name).toEqual('Belfast');
+    });
+});
+
--- src/ship.js
+++ src/ship.js
@@ -1,13 +1,22 @@
 
 class Ship {
     constructor(startingPort) {
-    this.startingPort = 'Liverpool';
+    this.currentPort = 'Liverpool';
     this.passengers = 0;
-    }
+    };
     boardShip() {
         this.passengers += 1;
-    }
+    };
+    setSail() {
+        this.currentPort = null;
+    };
+};
+
+class Port {
+    constructor(name) {
+        this.name = name;
+    };
 };
 
 
-module.exports = Ship;
+module.exports = { Ship, Port };
